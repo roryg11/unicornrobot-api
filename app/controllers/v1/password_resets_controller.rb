252a1a7dceb27541157cf4d40c98277192ddabc1
roryg11 <Rory.C.Grant@gmail.com>
2017-08-10 22:16:07 -0700

    password reset change stuff working

--- app/controllers/v1/password_resets_controller.rb
+++ app/controllers/v1/password_resets_controller.rb
@@ -8,15 +8,12 @@ module V1
       render json: true, status: 201
     end
 
-    def edit
-      @user = User.find_by_password_reset_token(params[:id])
-    end
-
     def update
-      @user = User.find_by_password_reset_token(params[:id])
-      if @user.password_reset_sent_at < 2.hours.ago
+      @user = User.find_by_reset_password_token(params[:id])
+      puts @user.reset_password_sent_at
+      if @user.reset_password_sent_at < 2.hours.ago
         render json: {error: t('password_resets_controller. Token has expired')}, status: :unprocessable_entity
-      elsif @user.update_attributes(params[:user])
+      elsif @user.update_attributes({password: params[:password]})
         render json: @user, status: 201
       end
     end
